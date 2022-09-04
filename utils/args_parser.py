78fed67b6af910e6646aa6073cdfd845d26a3539
avo888 <avoevodin8888@gmail.com>
2022-09-04 22:59:30 +0300

    updated gitignore updated dev dependencies added generate test logs command

---
+++ utils/args_parser.py
@@ -0,0 +1,45 @@
+"""
+Parse args passed to cli
+"""
+
+import argparse
+from argparse import Namespace
+import sys
+
+
+def get_parsed_args(params: list) -> Namespace:
+    """TODO"""
+    parser = argparse.ArgumentParser()
+
+    for param in params:
+        parser.add_argument(*param["names"], **param["kwargs"])
+
+    return parser.parse_args()
+
+
+def get_args_create_test_logs() -> Namespace:
+    """TODO"""
+    args_params = [
+        {
+            "names": ("--records", "-r"),
+            "kwargs": {
+                "help": "Amount of log records in each log file",
+                "required": True,
+                "type": int,
+            },
+        },
+        {
+            "names": ("--cnt", "-c"),
+            "kwargs": {
+                "help": "Log files quantity",
+                "required": True,
+                "type": int,
+            },
+        },
+    ]
+    args = get_parsed_args(args_params)
+    return args
+
+
+if __name__ == "__main__":
+    get_args_create_test_logs()
